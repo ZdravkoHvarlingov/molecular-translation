788e8e66bda2057e2fc5b3d188a3b59ea1466102
Mihail Mihaylov <mihail.mihaylov@ablera.com>
2021-05-23 18:19:32 +0300

    FIX: transformer encoder-decoder

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ from models.baseline.encoder_decoder_trainer import EncoderDecoderTrainer
 
 def train():
     trainer = EncoderDecoderTrainer()
-    dataframe = pd.read_csv("..\\bms-molecular-translation/extended_data_200.csv")
-    trainer.train(dataframe, 1000, plot_metrics=True)
+    dataframe = pd.read_csv("..\\bms-molecular-translation/extended_data_5000.csv")
+    trainer.train(dataframe, num_epochs=1000, plot_metrics=True, transformer=True)
 
 if __name__ == '__main__':
     train()
